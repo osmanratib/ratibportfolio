a98094960789f7a4c08babd1f10dc53ef52fff51
ratib54 <ratibsway@gmail.com>
2026-03-26 23:47:17 +0600

    add description in large screen

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ratibImage from "/Images/ratibimage.png"
 import EmailBar from '../EmailBar/EmailBar';
 import { LuCircleArrowOutUpRight } from "react-icons/lu";
+import { FiArrowRightCircle } from "react-icons/fi";
 
 const HeroSection = () => {
 
@@ -20,13 +21,13 @@ const HeroSection = () => {
 
     <div className="absolute bottom-0 left-1/2 -translate-x-1/2 z-20 flex justify-center w-full lg:w-[450px] ">
      <img
-      className='h-[400px] lg:h-[600px] w-[320px] lg:w-full'
+      className='h-[380px] lg:h-[600px] w-[320px] lg:w-full'
       src={ratibImage}
       alt="ratibImage"
      />
     </div>
 
-    <div className="content flex justify-center  absolute top-[10%] md:top-[20%] lg:top-[50%] w-full z-10">
+    <div id='content' className="content flex justify-center  absolute top-[10%] md:top-[20%] lg:top-[50%] w-full z-10">
      <h1
       // style={{
       //  WebkitTextStroke: "4px white",
@@ -36,6 +37,10 @@ const HeroSection = () => {
       osman ratib
      </h1>
     </div>
+      
+    <div className='relative top-[20%] left-[10%] hidden md:block' >
+     <h1 className='font-JosefinSans text-[17px] w-[400px] flex gap-3' > <span className='text-[20px]' ><FiArrowRightCircle /> </span>I’m a passionate web developer who thrives on tackling challenging problems and exploring innovative solutions </h1>
+   </div>
 
     <div className='lg:hidden md:hidden relative top-[30%] w-[400px] pl-20' >
      <h1 className='capitalize font-JosefinSans text-[17px] ' >I’m a passionate web developer who thrives on tackling challenging problems and exploring innovative solutions </h1>
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Sidebar = () => {
+ return (
+  <div>
+   <section>
+    <div className="content">
+     <h1>A Web Developer</h1>
+    </div> 
+    <div>
+    </div>
+   </section>
+  </div>
+ );
+};
+
+export default Sidebar;
